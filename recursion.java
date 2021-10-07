bef7dfa7aff2b57723c786bd64df2483b16d927a
Oohira Suguru <suguru.13579515@gmail.com>
2021-10-07 10:38:25 +0900

    Study CS

--- recursion.java
+++ recursion.java
@@ -3195,3 +3195,35 @@ class Solution{
         return result;
     }
 }
+
+
+// シャッフルの割合
+// easy
+// Beck は倉庫の整理係で、倉庫内にある荷物には全て商品番号が振られています。
+// ある日、地震が起きて棚にある荷物が全て落ちてしまい、Beck は急いで棚に戻したら荷物の順番がバラバラになりました。
+// 地震前の荷物 arr と地震後の荷物 shuffledArr が与えられるので、地震が起きた後に何%の荷物が移動したかを返す、shuffleSuccessRate という関数を定義してください。
+// 小数点以下に対しては切り捨ての処理を行ってください。また商品番号は一意であるとします。
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution{
+    public static int shuffleSuccessRate(int[] arr,int[] shuffledArr){
+        //ここから書きましょう
+
+        double total = arr.length;
+        double count = 0;
+
+        Map<Integer,Integer> a = new HashMap<Integer,Integer>();
+
+        for(int i = 0; i < shuffledArr.length; i++){
+            a.put(shuffledArr[i],i);
+        }
+
+        for(int i = 0 ; i < arr.length ; i++){
+            if(i != a.get(arr[i]))count += 1;
+        }
+
+        return (int)((count / total) * 100);
+    }
+}
